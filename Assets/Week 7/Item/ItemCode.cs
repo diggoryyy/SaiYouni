e0bc594f6b5958ff92a8b34488743d7345c2ab01
dandelt <hosituan1102@gmail.com>
2024-12-03 00:13:35 +0700

    w7

---
+++ Assets/Week 7/Item/ItemCode.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public enum ItemCode
+{
+    Gold = 0,
+    Gems = 1,
+    Crystals = 2
+}
---
+++ Assets/Week 7/Item/ItemProfileSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemProfile", menuName = "ScriptableObjects/ItemProfile", order = 1)]
+public class ItemProfileSO : ScriptableObject
+{
+    
+    public ItemCode itemCode;
+    public string itemName;
+    public bool isStackable = false;
+    public int maxStack = 99;
+}
